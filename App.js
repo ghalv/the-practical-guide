5314186554a5de228367d7a09ad32d58f3b50fbd
Gunnar K. Halvorsen <gunnar@ghalv.no>
2023-10-24 18:30:54 +0200

    lec. 27

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, View, Button, TextInput, FlatList } from 'react-native';
+import { GoalItem } from './components/GoalItem';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -28,11 +29,7 @@ export default function App() {
 	<FlatList
 	  data={courseGoals}
 	  renderItem={(itemData) => {
-	    return (
-	      <View style={styles.goalItem}>
-                <Text style={styles.goalText}>{itemData.item.text}</Text>
-	      </View>
-            );
+	    return <GoalItem />;
           }}
 	  keyExtractor={(item, index) => {
             return item.id;
@@ -68,13 +65,4 @@ const styles = StyleSheet.create({
   goalsContainer: {
 	flex: 5
   },
-  goalItem: {
-	margin: 8,
-	padding: 8,
-	borderRadius: 6,
-	backgroundColor: '#00263e',
-  },
-  goalText: {
-	color: 'white',
-  }
 });
